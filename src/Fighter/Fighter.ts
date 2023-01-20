f4f9e5e75a6aa6a8a8e27837b28a651cf4dbe011
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-01-19 21:40:48 -0300

    FEAT: Requisito 08 - Interface SimpleFighter

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,12 @@
+import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
+
+interface Fighter extends SimpleFighter {
+  defense: number;
+  energy?: Energy;
+
+  special?(enemy: Fighter): void;
+  levelUp(): void;
+}
+
+export default Fighter;
---
+++ src/Fighter/index.ts
@@ -0,0 +1,4 @@
+import Fighter from './Fighter';
+
+export default Fighter;
+export { default as SimpleFighter } from './SimpleFighter';
