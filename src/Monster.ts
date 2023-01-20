b524a4059b447f573ca9b629cba479ce627c2a51
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2023-01-20 17:11:11 -0300

    FEAT: Requisito 12 - Classe Dragon

--- src/Monster.ts
+++ src/Monster.ts
@@ -2,7 +2,7 @@ import SimpleFighter from './Fighter/SimpleFighter';
 import Character from './Character';
 
 class Monster implements SimpleFighter {
-  private _lifePoints: number;
+  protected _lifePoints: number;
   private _strength: number;
 
   constructor() {
